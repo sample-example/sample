fbeb5e77ec3d29b7108fb24797ea8f240d0bdf2b
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-11-19 17:06:21 +0900

    firebase追加

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAaoG-wS1ZVUOPS9r_9c4HMC7n5_hfnT9w",
+    authDomain: "example-8e74d.firebaseapp.com",
+    projectId: "example-8e74d",
+    storageBucket: "example-8e74d.appspot.com",
+    messagingSenderId: "607986070792",
+    appId: "1:607986070792:web:9bcdf32b2f7a68220a28bd",
+    measurementId: "G-02GHMPDHKS"
+};
+
+firebase.initializeApp(firebaseConfig);
